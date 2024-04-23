674a3b794bb938f3b90d1fe3be2e3cf85381d16d
azatyamanaev <iamanaev2001@gmail.com>
2024-04-23 12:41:20 +0300

    update to basic working version

---
+++ src/main/java/ru/itis/kpfu/ideacommittemplate/GitCommitTemplateAction.java
@@ -0,0 +1,31 @@
+package ru.itis.kpfu.ideacommittemplate;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.CheckinProjectPanel;
+import com.intellij.openapi.vcs.CommitMessageI;
+import com.intellij.openapi.vcs.VcsDataKeys;
+import com.intellij.openapi.vcs.ui.Refreshable;
+import org.jetbrains.annotations.NotNull;
+
+public class GitCommitTemplateAction extends AnAction {
+
+    private static final String TEMPLATE = "<Task number> <Description>\nChanges:\n- ";
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+        DataContext dc = e.getDataContext();
+        Project project = CommonDataKeys.PROJECT.getData(dc);
+        Refreshable panel = CheckinProjectPanel.PANEL_KEY.getData(dc);
+        final CommitMessageI commitMessageI =
+                (panel instanceof CommitMessageI) ? (CommitMessageI) panel : VcsDataKeys.COMMIT_MESSAGE_CONTROL.getData(dc);
+
+        if (commitMessageI != null && project != null) {
+           commitMessageI.setCommitMessage(TEMPLATE);
+        }
+    }
+}
